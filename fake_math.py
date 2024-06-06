0f43dd48cba28bcd7abf310bb78846b67ce06a62
KhasKelo <sir.yangulbaev@gmail.com>
2024-06-06 12:50:20 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,6 @@
+def devide(first, second):
+    if second != 0:
+        dev = first / second
+        return dev
+    else:
+        return 'Ошибка'
---
+++ true_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+def devide(first, second):
+    if second != 0:
+        dev = first / second
+        return dev
+    else:
+        return inf
